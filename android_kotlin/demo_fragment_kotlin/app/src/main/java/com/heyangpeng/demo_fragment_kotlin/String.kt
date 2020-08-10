5ec9a79f8e4fc2d6a0bac7f3b5342abdeab7c677
heyangpeng <heyangpeng8019@163.com>
2020-08-10 17:41:15 +0800

    添加常用运算符对应函数名

--- android_kotlin/demo_fragment_kotlin/app/src/main/java/com/heyangpeng/demo_fragment_kotlin/String.kt
+++ android_kotlin/demo_fragment_kotlin/app/src/main/java/com/heyangpeng/demo_fragment_kotlin/String.kt
@@ -3,5 +3,26 @@ package com.heyangpeng.demo_fragment_kotlin
 /**
  * 拓展String类
  */
-
+/**
+ * 运算符重载对应函数名
+ * a+b -> a.plus(b)
+ * a-b -> a.minus(b)
+ * a*b -> a.times(b)
+ * a/b -> a.div(b)
+ * a%b -> a.rem(b)
+ * a++ -> a.inc(b)
+ * a-- -> a.dec(b)
+ * +a -> a.unaryPlus(b)
+ * -a -> a.unaryMinus(b)
+ * !a -> a.not(b)
+ * a==b -> a.equals(b)
+ * a>b ->
+ * a<b ->
+ * a>=b -> a.compareTo(b)
+ * a<=b ->
+ * a..b -> a.rangeTo(b)
+ * a\[b] -> a.get(b)
+ * a\[b]=c -> a.set(b,c)
+ * a in b -> a.contains(b)
+ */
 operator fun String.times(n: Int) = repeat(n)
